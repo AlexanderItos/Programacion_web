20fd744cd36a2d7dc674f9f3c839c961ce174770
Mikelotto <92499822+Mikelotto@users.noreply.github.com>
2021-11-10 20:09:38 -0500

    Update actualizar_producto.php

--- php/actualizar_producto.php
+++ php/actualizar_producto.php
@@ -1,7 +1,7 @@
 <?php
 $servidor = "localhost";
 $usuario = "root";
-$password = "Chispita2006";
+$password = "12345678";
 $base = "bdunad42";
 
 $codigo = $_POST ['codProducto'];
@@ -13,7 +13,7 @@ $cantidad = $_POST ['cantidadComprada'];
 //Create connection
 $con = mysqli_connect($servidor, $usuario, $password,$base);
 if (!$con){
-	die("Fallo en la conexión: " . mysqli_connect_error());
+	die("Fallo en la conexiÃ³n: " . mysqli_connect_error());
 }
 
 $sql = "UPDATE tabla42 SET codigo = '$codigo', nombre = '$nombre', marca = '$marca', precio = '$precio', cantidad = '$cantidad' WHERE codigo = '$codigo'";
